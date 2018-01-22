b715204f0365b77c82aa0513a2ad4a25ab10c429
Kevin <kevnaf@gmail.com>
2018-01-22 15:19:49 -0800

    Constructor up and allPhotos array populated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
 'use strict';
+
+// Devlare variables
+var voteLimit = 5;
+var totalVoteCount = 0;
+var photosSeen = 0;
+var imgElC1 = document.getElementById('choiceOne');
+var imgElC2 = document.getElementById('choiceTwo');
+var imgElC3 = document.getElementById('choiceThree');
+PhotoChoice.allPhotos = [];
+
+// Create object constructor
+function PhotoChoice(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  this.voteCount = 0;
+  this.lastThree = false;
+  PhotoChoice.allPhotos.push(this);
+}
+
+// Create instances of PhotoChoices
+new PhotoChoice('bag', 'img/bag.jpg');
+new PhotoChoice('banana', 'img/banana.jpg');
+new PhotoChoice('bathroom', 'img/bathroom.jpg');
+new PhotoChoice('boots', 'img/boots.jpg');
+new PhotoChoice('breakfast', 'img/breakfast.jpg');
+new PhotoChoice('bubblegum', 'img/bubblegum.jpg');
+new PhotoChoice('chair', 'img/chair.jpg');
+new PhotoChoice('cthulhu', 'img/cthulhu.jpg');
+new PhotoChoice('dog-duck', 'img/dog-duck.jpg');
+new PhotoChoice('dragon', 'img/dragon.jpg');
+new PhotoChoice('pen', 'img/pen.jpg');
+new PhotoChoice('pet-sweep', 'img/pet-sweep.jpg');
+new PhotoChoice('scissors', 'img/scissors.jpg');
+new PhotoChoice('shark', 'img/shark.jpg');
+new PhotoChoice('sweep', 'img/sweep.jpg');
+new PhotoChoice('tauntaun', 'img/tauntaun.jpg');
+new PhotoChoice('unicorn', 'img/unicorn.jpg');
+new PhotoChoice('usb', 'img/usb.jpg');
+new PhotoChoice('water-can', 'img/water-can.jpg');
+new PhotoChoice('wine-glass', 'img/wine-glass.jpg');
+
